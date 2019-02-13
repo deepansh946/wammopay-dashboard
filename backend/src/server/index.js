67980f8fbbb144d1fc081e5c86d2f5d408c7bb20
Deepansh Bhargava <deepansh940@gmail.com>
2019-02-13 22:27:44 +0530

    SSR

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -3,6 +3,11 @@ import bodyParser from 'body-parser';
 import middleware from './middleware/index';
 import routes from './routes/index';
 
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { SheetsRegistry } from 'react-jss/lib/jss';
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -26,6 +31,38 @@ middleware(app);
 routes(app);
 
 app.use(express.static('public'));
+
+app.use((req, res) => {
+  const SheetRegistry = new SheetsRegistry();
+
+  const generate = createGenerateClassName();
+
+  const html = renderToString(
+    <JssProvider registry={SheetRegistry} generateClassName={generate}>
+      <App />
+    </JssProvider>
+  );
+
+  const css = SheetRegistry.toString();
+
+  res.send(`
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wammo Pay Dashboard</title>
+    <style id='jss-styles'>${css}</style>
+  </head>
+  <body>
+    <div id="app">${html}</div>
+    <script src="main.js" async></script>
+    ${dev ? '<script src="/reload/reload.js" async></script>' : ''}
+  </body>
+  </html>
+  `);
+});
 
 app.listen(PORT, error => {
   if (error) {
